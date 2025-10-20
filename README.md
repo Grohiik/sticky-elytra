37bcdb94e18bf0afc41a23c019b668bb27016a8e
Linnéa <heartbrrrrr@protonmail.com>
2025-10-20 21:56:18 +0200

    Update to 1.21.10
    Has a bug where offhand and equipment gets droped.....

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Well do I have a solution for you!!!!
 The only foolproof way to make sure you never lose your trusty wings ever again.  
 With sticky elytra your wings stay on your back through thick and thin, no matter what you are up to.  
 Simply put our univeral jar in your modfolder and your elytra will never leave you again, unlike my husband.
+
+
+# Awesome instructions for updating
+https://epicbunny.dev/2025/08/02/fabric-modding-updating-our-mod-from-minecraft-1-21-6-to-1-21-8/
--- src/main/java/heartbrrr/sticky_elytra/mixin/PlayerManagerMixin.java
+++ src/main/java/heartbrrr/sticky_elytra/mixin/PlayerManagerMixin.java
@@ -1,17 +1,13 @@
 package heartbrrr.sticky_elytra.mixin;
 
-import com.google.common.collect.ImmutableList;
 import net.minecraft.entity.Entity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
@@ -19,21 +15,7 @@ public class PlayerManagerMixin {
     private void transferInventory(ServerPlayerEntity oldplayer, boolean alive, Entity.RemovalReason removalReason,
                                    CallbackInfoReturnable<ServerPlayerEntity> cir) {
         ServerPlayerEntity newPlayer = cir.getReturnValue();
-        List<DefaultedList<ItemStack>> oldInventorylist =
-            ImmutableList.of(oldplayer.getInventory().main, oldplayer.getInventory().armor,
-                             oldplayer.getInventory().offHand);
-        List<DefaultedList<ItemStack>> newInventorylist =
-            ImmutableList.of(newPlayer.getInventory().main, newPlayer.getInventory().armor,
-                             newPlayer.getInventory().offHand);
-
-        for (int i = 0; i < 3; i++) {
-            DefaultedList<ItemStack> oldInventory = oldInventorylist.get(i);
-            DefaultedList<ItemStack> newInventory = newInventorylist.get(i);
-
-            for (int j = 0; j < oldInventory.size(); j++) {
-                newInventory.set(j, oldInventory.get(j));
-            }
-        }
+        newPlayer.getInventory().clone(oldplayer.getInventory());
 
         cir.setReturnValue(newPlayer);
     }
